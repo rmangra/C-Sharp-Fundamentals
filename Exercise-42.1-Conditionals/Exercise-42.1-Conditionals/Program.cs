28a1f289914a98308304832761a359d4d77ca608
rmangra <richard.mangra@gmail.com>
2017-11-19 15:52:02 -0500

    Program that validates that a number entered by user is between 1 to 10.

---
+++ Exercise-42.1-Conditionals/Exercise-42.1-Conditionals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_Conditionals_42._1
+{
+    class Program
+    {
+        // Write a program and ask the user to enter a number between 1 to 10. If the user enters 
+        // a valid number, display "Valid" on the console. Otherwise, display "Invalid".
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number between 1 to 10: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
