aba0a2e0c507990faefbc075f44298baf3b6f972
rmangra <richard.mangra@gmail.com>
2017-11-19 07:55:22 -0500

    Logic operators

---
+++ Operators.Program/Operators.Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine(a + b * c);
+            Console.WriteLine((a + b) * c);
+            Console.WriteLine(c > b || c > a);
+
+        }
+    }
+}
