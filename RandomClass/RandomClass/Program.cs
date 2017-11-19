7b45d22934021fe5180643ec5d13816c2d04ace7
rmangra <richard.mangra@gmail.com>
2017-11-19 17:28:53 -0500

    Used the random class to code a password generator

---
+++ RandomClass/RandomClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+            char[] buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i]  = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
