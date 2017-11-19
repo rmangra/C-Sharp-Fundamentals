f715c1caba247fe28b8caf8eee076d8ee0d78ea2
rmangra <richard.mangra@gmail.com>
2017-11-19 15:55:24 -0500

    Program that asks the user to enter the width and height of an image, and determines if the image is landscape or portrait.

---
+++ Exercise-42.3-Conditionals/Exercise-42.3-Conditionals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_42._3_Conditionals
+{
+    class Program
+    {
+        // Write a program and ask the user to enter the width and height of an image.
+        // Then tell if the image is landscape or portrait.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter image width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter image height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            if (width > height)
+            {
+                Console.WriteLine("The image is in landscape orientation.");
+            }
+            else
+            {
+                Console.WriteLine("The image is in portrait orientation.");
+            }
+        }
+    }
+}
