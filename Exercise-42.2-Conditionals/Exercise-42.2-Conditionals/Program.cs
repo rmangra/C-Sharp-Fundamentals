3ad2249778661b9595f136e111f71ef55f68bce8
rmangra <richard.mangra@gmail.com>
2017-11-19 15:53:47 -0500

    Program that takes two numbers from the console and displays the maximum of the two.

---
+++ Exercise-42.2-Conditionals/Exercise-42.2-Conditionals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_Conditionals_42._2
+{
+    class Program
+    {
+        // Write a program which takes two numbers from the console 
+        // and displays the maximum of the two.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a second number: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("The max value is " + num1);
+            }
+            else
+            {
+                Console.WriteLine("The max value is " + num2);
+            }
+        }
+    }
+}
