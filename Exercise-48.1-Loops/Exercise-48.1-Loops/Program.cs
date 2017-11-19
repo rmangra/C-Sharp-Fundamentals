4229adf99b397b186b5e37bc2ce7fdf6e06097ad
rmangra <richard.mangra@gmail.com>
2017-11-19 17:41:10 -0500

    Program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.

---
+++ Exercise-48.1-Loops/Exercise-48.1-Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise_48._1_Loops
+{
+    class Program
+    {
+        // Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+        // Display the count on the console.
+        static void Main(string[] args)
+        {
+            var count = 0;
+            for (var i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
