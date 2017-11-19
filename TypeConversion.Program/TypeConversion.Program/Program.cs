f377318b07311158f7dbb62be3dba57f137e9359
rmangra <richard.mangra@gmail.com>
2017-11-18 22:37:34 -0500

    Type Conversion

---
+++ TypeConversion.Program/TypeConversion.Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+
+
+        }
+    }
+}
