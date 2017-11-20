28554c0765da78b0e751ffc4206447597e34958f
rmangra <richard.mangra@gmail.com>
2017-11-20 14:18:37 -0500

    Program that allows the user to summarize a large block of text using ellipsis (...).  Created a reusable method to achieve this and then moved it into its own class called, StringUtility.

---
+++ SummarizingText/SummarizingText/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long piece of text.";
+            var summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+        
+    }
+}
