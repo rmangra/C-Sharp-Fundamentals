e3cb9a8e57b5ac758ebabcb497f3088eeb54bafb
rmangra <richard.mangra@gmail.com>
2017-11-20 08:56:19 -0500

    Examples using Lists

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() {1, 2, 3, 4};
+            numbers.Add(1);
+            numbers.AddRange(new int[3] {5, 6, 7});
+        }
+    }
+}
