292d3712d01430cdbe943e4242985ff1005abb5d
rmangra <richard.mangra@gmail.com>
2017-11-19 22:28:07 -0500

    Program picks a random number between 1 and 10, and gives the user 4 chances to guess the number.

---
+++ Exercise-48.4-Loops/Exercise-48.4-Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_48._4_Loops
+{
+    class Program
+    {
+        // Write a program that picks a random number between 1 and 10. 
+        // Give the user 4 chances to guess the number. If the user guesses the number, display “You won";
+        // otherwise, display “You lost".
+        
+        static void Main(string[] args)
+        {
+            var number = new Random().Next(1, 10);
+            const int chances = 4;
+            for (var i = chances; i > 0; i--)
+            {
+                Console.Write("You have " + i + " chance(s) to guess the secret number (between 1 to 10): ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+                
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    Console.WriteLine("The secret number is " + number);
+                    return;
+                }
+            }
+            Console.WriteLine("You lost!");
+            Console.WriteLine("The secret number is " + number);
+        }
+    }
+}
