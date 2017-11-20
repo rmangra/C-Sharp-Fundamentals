0ed880a8965e88408f62835040a99087e4db99e3
rmangra <richard.mangra@gmail.com>
2017-11-20 15:52:56 -0500

    Program that converts two or more words entered by the user into a PascalCase styled variable name.

---
+++ Exercise-67.4-Working-with-Text/Exercise-67.4-Working-with-Text/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_67._4_Working_with_Text
+{
+    class Program
+    {
+        // Write a program and ask the user to enter a few words separated by a space. Use the words to 
+        // create a variable name with PascalCase convention. For example, if the user types: 
+        // "number of students", display "NumberOfStudents". Make sure the program is not dependent on 
+        // the casing of the input. So if the input is "NUMBER OF STUDENTS", it should still display 
+        // "NumberOfStudents". If the user doesn't supply any words, display "Error".
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a two or more words: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
+    }
+}
