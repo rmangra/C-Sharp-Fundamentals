7a98a57cb9e5b21709244ba5798824d6747ef16c
rmangra <richard.mangra@gmail.com>
2017-11-20 10:36:48 -0500

    Continously prompt user to enter numbers or quit.

---
+++ Exercise-55.4-Arrays-and-Lists/Exercise-55.4-Arrays-and-Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_55._4_Arrays_and_Lists
+{
+    // Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+    // The list of numbers may include duplicates. Display the unique numbers that the user has entered.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number (or 'Quit' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+        }
+    }
+}
