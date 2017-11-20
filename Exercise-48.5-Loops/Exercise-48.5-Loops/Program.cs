7c16a59c6d0de27d52266bfde7d1a8cb53651f5f
rmangra <richard.mangra@gmail.com>
2017-11-20 07:23:30 -0500

    Program that returns the max number of a series of numbers entered by the user.

---
+++ Exercise-48.5-Loops/Exercise-48.5-Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_48._5_Loops
+{
+    class Program
+    {
+        // Write a program and ask the user to enter a series of numbers separated by comma.
+        // Find the maximum of the numbers and display it on the console.
+        // For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter comma separated numbers: ");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            // Assume the first number is the max 
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                    max = number;
+            }
+
+            Console.WriteLine("Max is " + max);
+        }
+    }
+}
