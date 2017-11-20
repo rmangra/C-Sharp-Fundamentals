86e16bbb3572c916b2c52249dd7cb4b9af1e939c
rmangra <richard.mangra@gmail.com>
2017-11-20 09:38:00 -0500

    Prompt user to enter their name and use array to reverse it.

---
+++ Exercise-55.2-Arrays-and-Lists/Exercise-55.2-Arrays-and-Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_55._2_Arrays_and_Lists
+{
+    class Program
+    {
+        // Write a program and ask the user to enter their name. 
+        // Use an array to reverse the name and then store the result in a new string. 
+        // Display the reversed name on the console.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+        }
+    }
+}
