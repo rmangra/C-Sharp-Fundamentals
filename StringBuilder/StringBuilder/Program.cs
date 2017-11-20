bd3b05f3b1a58b62a44860d85995ee355d6ea4c1
rmangra <richard.mangra@gmail.com>
2017-11-20 14:32:43 -0500

    Examples using StringBuilder

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new System.Text.StringBuilder("Hello World");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+            
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+            
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First character: " + builder[0]);
+        }
+    }
+}
