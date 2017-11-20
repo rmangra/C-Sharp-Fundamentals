e279fb50fc8d21b62dc67c0a891359a84404a945
rmangra <richard.mangra@gmail.com>
2017-11-20 16:23:03 -0500

    Directory methods

---
+++ Directory-and-DirectoryInfo/Directory-and-DirectoryInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory_and_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+
+        }
+    }
+}
