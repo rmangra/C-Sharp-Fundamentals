88dc245caaed8dd8c22ff2334e184144718444ac
rmangra <richard.mangra@gmail.com>
2017-11-20 10:23:49 -0500

    Write a program and ask the user to enter 5 numbers.

---
+++ Exercise-55.3-Arrays-and-Lists/Exercise-55.3-Arrays-and-Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_55._3_Arrays_and_Lists
+{
+    // Write a program and ask the user to enter 5 numbers. If a number has been previously entered,
+    // display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers,
+    // sort them and display the result on the console.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (numbers.Count < 5)
+            {
+                Console.Write("Enter five different numbers (one per line): ");
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine("You've previously entered " + number);
+                    continue;
+                }
+
+                numbers.Add(number);
+            }
+
+        }
+    }
+}
