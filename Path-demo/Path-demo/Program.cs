a4c86f0494f319a0f2fc8f27b7c21c32f45ccccf
rmangra <richard.mangra@gmail.com>
2017-11-20 16:39:01 -0500

    Examples using Path methods

---
+++ Path-demo/Path-demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Path_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\temp\HelloWorld.sln";
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
