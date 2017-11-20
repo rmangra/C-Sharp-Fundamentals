f8951d0ade69c002ec31549f170731efcb1fbee3
rmangra <richard.mangra@gmail.com>
2017-11-20 09:30:10 -0500

    Program that prompts user to enter different names and returns the number of people who like your post.

--- Exercise-55.1-Arrays-and-Lists/Exercise-55.1-Arrays-and-Lists/Program.cs
+++ Exercise-55.1-Arrays-and-Lists/Exercise-55.1-Arrays-and-Lists/Program.cs
@@ -31,7 +31,14 @@ namespace Exercise_55._1_Arrays_and_Lists
                     break;
                 names.Add(name);
             }
-            
+            if (names.Count == 1)
+                Console.WriteLine("{0} likes your post.", names[0]);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+            else if (names.Count > 2)
+                Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
+            else
+                Console.WriteLine("No Likes");
         }
     }
 }
